aaa41912347484c2eedc783cb3d0019ea055000c
Jason Wee <peichieh@gmail.com>
2019-07-30 14:46:36 +0800

    changed to metrics to support multiple metrics per script

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 class Config:
-    def __init__(self, description, enable, script, metric, value, operator, threshold_operator, alert_value):
+    def __init__(self, description, enable, script, metrics, value, operator, threshold_operator, alert_value):
         self.description = description
         self.enable = enable
         self.script = script
-        self.metric = metric
+        self.metrics = metrics
         self.value = value
         self.operator = operator
         self.threshold_operator = threshold_operator
         self.alert_value = alert_value
 
     def __str__(self):
-        return "description: %s, enable: %s, script : %s, metric: %s, value: %s, operator: %s, threshold_operator: %s, alert_value: %s" % (self.description, self.enable, self.script, self.metric, self.value, self.operator, self.threshold_operator, self.alert_value)
+        return "description: %s, enable: %s, script : %s, metrics: [%s], value: %s, operator: %s, threshold_operator: %s, alert_value: %s" % (self.description, self.enable, self.script, ",".join(self.metrics), self.value, self.operator, self.threshold_operator, self.alert_value)
 
     def __repr__(self):
         return self.__str__()
